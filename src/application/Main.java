5ebdb6501b37fff3beca66f5c0885728de983a4f
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-04-13 23:28:21 -0400

    Added reading file locations

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,62 @@
 package application;
 
-/**
- * Application's main
- */
-public class Main {
-    public static void main(String[] args){
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
-    }
+
+public class Main {
+
+	public static void main(String[] args) throws FileNotFoundException, IOException  {
+		// TODO Auto-generated method stub
+		
+		System.out.println("-------Training-------\n");
+		readTrainFiles();
+		System.out.println("\n-------Testing-------\n");
+		readTestFiles();
+
+	}
+	
+	
+	public static void readTrainFiles() throws FileNotFoundException, IOException {
+
+		//reading the directory of files
+		try(Stream<Path> paths = Files.walk(Paths.get("data/train"))) {
+		    paths.forEach(filePath -> {
+		    	
+		        if (Files.isRegularFile(filePath)) {
+		        	
+		            //creating a Path object to store the file's name as it increments through
+		            String fileName = filePath.getFileName().toString();
+		            
+		        System.out.println(fileName);
+		            
+		        }
+		    });
+		} 
+		
+	}
+	
+	public static void readTestFiles() throws FileNotFoundException, IOException {
+
+		//reading the directory of files
+		try(Stream<Path> paths = Files.walk(Paths.get("/data/test"))) {
+		    paths.forEach(filePath -> {
+		    	
+		        if (Files.isRegularFile(filePath)) {
+		        	
+		            //creating a Path object to store the file's name as it increments through
+		            String fileName = filePath.getFileName().toString();
+		            
+		        System.out.println(fileName);
+		            
+		        }
+		    });
+		} 
+		
+	}
+
 }
