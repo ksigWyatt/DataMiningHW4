ec5b184465da25ced5b7d2510d7112355d748332
Shawn Reece <shawnjreece21@hotmail.com>
2017-04-18 22:45:25 -0400

    Cherry picked from code from local
    Brought in your changes from local branch resolved merge conflicts

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -2,6 +2,7 @@ package controller;
 
 import models.WordCounts;
 import services.NaiveBayes;
+import services.KNNAlgo;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -12,17 +13,26 @@ import java.io.FileNotFoundException;
 public class Controller {
     WordCounts wordCount;
     NaiveBayes navieBayes;
+    KNNAlgo knn;
     public Controller(WordCounts counts){
         wordCount = counts;
     }
 
     public void runApp(){
         try {
+            wordCount.wordsSet();
+            navieBayes = new NavieBayesSpamAlgo(wordCount.getSpamWordCount(), wordCount.getHamWordCount());
+            knn = new KNNAlgo(wordCount.getSpamWordCount(), wordCount.getHamWordCount());
             File folder = new File("data/train");
             wordCount.wordsSet(folder.listFiles());
             navieBayes = new NaiveBayes(wordCount);
             navieBayes.classify();
             navieBayes.results();
+
+            for(File file : folder.listFiles()) {
+                knn.classifyKNN(file);
+                break; // just see one file
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
--- src/services/KNNAlgo.java
+++ src/services/KNNAlgo.java
@@ -1,7 +1,38 @@
 package services;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.math.*;
+
 /**
  * Created by My Surface on 4/17/2017.
  */
 public class KNNAlgo {
+	
+    HashMap<String, Integer> spam;
+    HashMap<String, Integer> ham;
+    int totalHam;
+    int totalSpam;
+    public static final int HAM = 0;
+    public static final int SPAM = 1;
+	
+	 public KNNAlgo(HashMap<String, Integer> spam, HashMap<String, Integer> ham) {
+	        this.ham = ham;
+	        totalHam = ham.size();
+	        this.spam = spam;
+	        totalSpam = spam.size();
+	    }
+	 
+	 public Double classifyKNN(File file)throws FileNotFoundException {
+		 Double distance = 0.0;
+		 
+		 //this is wher ewe would calculate the KNN nearest neighbor and return it's distance
+		 distance = Math.sqrt(Math.pow((totalHam - totalSpam), 2));
+		 
+		 return distance;
+	 }
+
 }
