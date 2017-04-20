2c5226b3d31e638f6bf24bd2c4b22e2f2d1bb92d
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-04-20 12:32:35 -0400

    KNN Training almost working

--- src/models/KNNModel.java
+++ src/models/KNNModel.java
@@ -1,5 +1,32 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class KNNModel {
+	
+	HashMap<String, Integer> terms;
+	double distance = 0;
+	
+	
+	public KNNModel() {
+		terms = new HashMap<>();
+	}
+	
+	public double calculateDistance() {
+	
+		
+		return distance;
+	}
+
+
+	public void addWord(String word) {
+		terms.put(word, (terms.getOrDefault(word,0) + 1));		
+	}
+	
+	public int getWordOccurance(String word) {
+		return terms.get(word);
+		
+	}
 
 }
